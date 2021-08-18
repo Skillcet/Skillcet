951f794ff2c9b932e5640f8ab10e6ecbf989e650
santhosh-cloud <coe19b041@iiitdm.ac.in>
2021-08-18 20:24:17 +0530

    active links

--- pages/nra.js
+++ pages/nra.js
@@ -1,5 +1,7 @@
 import Layout from "../components/layout";
-import Body from "../components/Body";
+import loadable from "@loadable/component";
+
+const Body = loadable(() => import("../components/Body"));
 export default function Home() {
   return (
     <Layout pageTitle="NRA">
