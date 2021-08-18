cafc9326120358a5c9b2588d4d1a2e9c42b27e6c
santhosh-cloud <coe19b041@iiitdm.ac.in>
2021-08-18 12:49:25 +0530

    mobile footer -1

--- components/MobileHeader.js
+++ components/MobileHeader.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function MobileHeader() {
   return (
-    <div className="bottom-0  w-full flex justify-center items-center md:hidden fixed overflow-hidden h-14 bg-white shadow-md">
-      <div className="grid grid-cols-4 justify-center w-full items-center text-center">
-        <div>Item</div>
-        <div>Item</div>
-        <div>Item</div>
-        <div>Item</div>
+    <div className="bottom-0  w-full flex justify-center items-start md:hidden fixed overflow-hidden h-14 bg-white shadow-md">
+      <div className="grid grid-cols-4 justify-center w-full items-strech text-center h-12 px-2 ">
+        <div className=" bg-blue-500 pt-4 rounded-b-lg text-white">Item</div>
+        <div className=" pt-4 ">Item</div>
+        <div className=" pt-4 ">Item</div>
+        <div className=" pt-4 ">Item</div>
       </div>
     </div>
   );
