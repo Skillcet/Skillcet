8543e089b9b0d49c63d181a36c85d6ff152a3689
santhosh-cloud <coe19b041@iiitdm.ac.in>
2021-08-18 13:09:25 +0530

    mobile-footer added text

--- components/MobileHeader.js
+++ components/MobileHeader.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function MobileHeader() {
   return (
-    <div className="bottom-0  w-full flex justify-center items-start md:hidden fixed overflow-hidden h-14 bg-white shadow-md">
+    <div className="bottom-0  w-full flex justify-center items-start md:hidden fixed overflow-hidden h-14 shadow-mobile-footer bg-white shadow-md">
       <div className="grid grid-cols-4 justify-center w-full items-strech text-center h-12 px-2 ">
-        <div className=" bg-blue-500 pt-4 rounded-b-lg text-white">Item</div>
-        <div className=" pt-4 ">Item</div>
-        <div className=" pt-4 ">Item</div>
-        <div className=" pt-4 ">Item</div>
+        <div className=" bg-blue-500 pt-4 rounded-b-lg text-white">Home</div>
+        <div className=" pt-4 ">NRA</div>
+        <div className=" pt-4 ">SkillCET</div>
+        <div className=" pt-4 ">New Batches</div>
       </div>
     </div>
   );
