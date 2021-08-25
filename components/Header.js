a3956f390b5ec258ae2c4b2e30fb79518852f79e
santhosh-cloud <56725056+santhosh-cloud@users.noreply.github.com>
2021-08-25 21:14:18 +0530

    icons and header

--- components/Header.js
+++ components/Header.js
@@ -1,36 +1,42 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/solid";
+import { SearchIcon, UserIcon } from "@heroicons/react/solid";
 
 function Header() {
   return (
-    <header className="top-0 z-50 sticky w-full grid grid-cols-4 h-16 md:h-18 items-center py-0 bg-white shadow-md">
-      {/* logo */}
-      <div className="relative flex-shrink-0   flex justify-around items-center">
-        <Link href="/">
-          <a>
-            <p>Logo</p>
-          </a>
-        </Link>
+    <header className="top-0 z-50 sticky text-sm 2xl:text-base w-full flex justify-evenly h-16 md:h-18 items-center py-0 bg-white shadow-md">
+      <div className="w-1/4 md:w-1/2 flex justify-around">
+        {/* logo */}
+        <div className="relative flex-auto flex justify-around items-center">
+          <Link href="/">
+            <a>
+              <p>Logo</p>
+            </a>
+          </Link>
+        </div>
+        {/* menu */}
+        <div className=" hidden flex-auto md:flex flex-row gap-1 items-center justify-evenly">
+          {/* menu items(3) */} <p>National Recruitment Agency</p>
+          {/* menu items(3) */} <p>Skillcet</p>
+          {/* menu items(3) */} <p>New Batches</p>
+        </div>
       </div>
-      {/* menu */}
-      <div className=" hidden md:flex flex-row gap-1 items-center  justify-around">
-        {/* menu items(3) */} <p>Menu</p>
-      </div>
-
-      {/* search */}
-      <div className="flex w-4/5 py-2 px-2 rounded-lg  col-span-2 md:col-span-1 bg-blue-100 ">
-        <SearchIcon className="object-contain h-7 pr-4 text-gray-600 " />
-        <input
-          type="text"
-          placeholder="Search For Something"
-          className="bg-blue-100 placeholder-gray-400 w-full outline-none border-none"
-        />
-      </div>
-      {/* user icons*/}
-      <div className="px-4  flex items-center justify-around">
-        <p>Login</p>
+      <div className="w-3/4 md:w-1/2 flex justify-around">
+        {/* search */}
+        <div className="flex w-3/4 py-2 px-2 rounded-lg  bg-blue-100 ">
+          <SearchIcon className="object-contain h-6 pr-4 text-gray-600 " />
+          <input
+            type="text"
+            placeholder="Search For Something"
+            className="bg-blue-100 w-full placeholder-gray-400 outline-none border-none"
+          />
+        </div>
+        {/* user icons*/}
+        <div className="flex flex-auto items-center justify-evenly">
+          <p className="hidden md:flex">Student Login</p>
+          <UserIcon className="h-6 object-contain" />
+        </div>
       </div>
     </header>
   );
--- components/Menu.js
+++ components/Menu.js
@@ -8,8 +8,8 @@ function Menu() {
     <div
       className={
         router.pathname == item.link
-          ? "bg-blue-500 py-2 rounded-r-3xl my-2 px-4 text-white"
-          : "my-4 px-4"
+          ? "bg-blue-500 rounded-r-3xl text-white my-2 px-4 py-3"
+          : "my-2 px-4 py-3"
       }
       key={item.key}
     >
@@ -19,7 +19,7 @@ function Menu() {
     </div>
   ));
   return (
-    <div className="bg-white w-full h-full ">
+    <div className="bg-white w-full text-sm md:text-base h-full ">
       <div className="h-60 w-full my-4 bg-green-200"></div>
       <div className=" border-t-2 border-b-2 my-8 border-black mx-auto">
         {menuItems}
