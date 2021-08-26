7eb389491a79bf9e714b326126811be6c9106dc0
santhosh-cloud <56725056+santhosh-cloud@users.noreply.github.com>
2021-08-26 21:01:36 +0530

    logo-added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,14 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      screens: {
-        "3xl": "1700px",
-      },
+    extend: {},
+    screens: {
+      sm: "640px",
+      md: "950px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1700px",
     },
   },
   variants: {
