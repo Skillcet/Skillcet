fd23bbf9bad4b2b62484ebf3f96b1a17d0a3680e
Skillcet <skillsntools.web@gmail.com>
2021-09-02 15:37:27 +0530

    font size -20px

--- components/Header_2.js
+++ components/Header_2.js
@@ -8,15 +8,15 @@ function Header_2({ isOpen, SetIsOpen }) {
   return (
     <header className="sticky top-0 z-50 h-[64px] w-full shadow-md">
       <div className="h-full bg-white p-[8px] ">
-        <div className="flex items-center justify-between h-full lg:mx-[48px]">
+        <div className="flex items-center h-full lg:mx-[26px]">
           {/* Header-Left */}
-          <div className="flex items-center ">
+          <div className="flex items-center justify-between ">
             {/* Header-Left__Items */}
             <button
               onClick={() => SetIsOpen(!isOpen)}
-              className="hidden lg:inline-flex"
+              className="hidden lg:inline-flex "
             >
-              <div className="hover:bg-blue-300 hover:bg-opacity-40 p-2 mr-1 rounded-full cursor-pointer">
+              <div className="hover:bg-blue-300 hover:bg-opacity-40 p-2 mr-[32px] rounded-full cursor-pointer">
                 <MenuIcon className="text-gray-600 h-6 w-6" />
               </div>
             </button>
@@ -36,11 +36,11 @@ function Header_2({ isOpen, SetIsOpen }) {
           </div>
 
           {/* Header-Middle */}
-          <div className="h-full flex items-center justify-around w-full text-[20px]">
+          <div className="h-full flex items-center justify-around w-full text-sm">
             {/* Header-Middle__Items */}
-            <div>NRA</div>
-            <div>SkillCET</div>
-            <div>New Batches</div>
+            <div className="hidden md:inline-flex">NRA</div>
+            <div className="hidden md:inline-flex">SkillCET</div>
+            <div className="hidden md:inline-flex">New Batches</div>
             <div className="h-full flex items-center bg-blue-100 rounded-lg px-3 w-11/12 sm:w-4/5 max-w-3xl">
               <SearchIcon className="h-6 text-gray-600" />
               <input
@@ -51,10 +51,10 @@ function Header_2({ isOpen, SetIsOpen }) {
             </div>
           </div>
           {/* Header-Right */}
-          <div className=" flex sm:w-[180px] items-center justify-between">
+          <div className=" flex sm:w-[200px] items-center justify-between">
             {/* Header-Right__Items */}
             <div className="text-sm hidden sm:inline-flex">Student Login</div>
-            <div>
+            <div className="pr-2 ml-[32px]">
               <AccountIcon className="h-6 w-6 text-gray-500 cursor-pointer" />
             </div>
           </div>
