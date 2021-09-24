5e53530218b30334f6d1ef61173c94fca4dfe3da
Skillcet <skillsntools.web@gmail.com>
2021-09-24 15:28:48 +0530

    tab changes

--- pages/sampletest.js
+++ pages/sampletest.js
@@ -1,9 +1,7 @@
 import React from "react";
-import Logo from "../public/logo1.svg";
-import Image from "next/image";
-import ColorTabs from "../components/SampleTabs";
+
 const sampletest = () => {
-  return <ColorTabs />;
+  return <>Sample Test Page</>;
 };
 
 export default sampletest;
